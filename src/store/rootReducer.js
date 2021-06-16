42bf6445dbb9d5bfe63d6265883352d12d187120
fatihsahin3 <fatihsahin3@gmail.com>
2021-06-16 23:26:38 +0200

    First Commit

---
+++ src/store/rootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+import cartReducer from "./reducers/cartReducer";
+
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  //user:userReducer
+});
+
+export default rootReducer;
